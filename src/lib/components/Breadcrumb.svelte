78d648790977a78399963e1565ced1d75ebe080b
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-05-17 00:13:56 +0900

    パンくずリストを追加する

---
+++ src/lib/components/Breadcrumb.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  const baseURL = "https://nagutabby.uk";
+  const partsOfPathname = $page.url.pathname.substring(1).split("/");
+  let tmpURL: string;
+  export let title: string;
+
+  function clearTmpURL() {
+    tmpURL = baseURL;
+  }
+  function addPathToTmpURL(partOfPathname: string) {
+    tmpURL = tmpURL + "/" + partOfPathname;
+  }
+</script>
+
+<nav aria-label="breadcrumb">
+  <ul>
+    <li>
+      <a href={baseURL}>Home</a>
+    </li>
+    {#each partsOfPathname as partOfPathname, i}
+      {#if i === partsOfPathname.length - 1}
+        <li>
+          {title}
+        </li>
+      {:else}
+        {clearTmpURL()}
+        <li>
+          {#each partsOfPathname.slice(0, i) as partOfPathname}
+            {addPathToTmpURL(partOfPathname)}
+          {/each}
+          <a href={tmpURL}>{partOfPathname}</a>
+        </li>
+      {/if}
+    {/each}
+  </ul>
+</nav>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+  import Breadcrumb from "$lib/components/Breadcrumb.svelte";
   import type { PageData } from "./$types";
   export let data: PageData;
 </script>
 
+<Breadcrumb title={data.title} />
+
 <article>
   {@html data.body}
 </article>
+
+<style>
+  article {
+    margin-top: 0;
+  }
+</style>
