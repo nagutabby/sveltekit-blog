351d3f036492337b7e90dfac246cb90a45f21ee1
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-05-17 18:25:42 +0900

    仮のTOCを作成する

--- src/lib/components/Toc.svelte
+++ src/lib/components/Toc.svelte
@@ -1,35 +1,15 @@
 <script lang="ts">
   export let toc: { text: any; id: string; name: string }[];
-
-  let tmpItemName = "";
-  let count = 0;
-  function clearCount() {
-    count = 0;
-  }
-  function incrementCount() {
-    count += 1;
-  }
-  function updateTmpItemName(itemName: string) {
-    tmpItemName = itemName;
-  }
 </script>
 
 <article>
   <details>
     <summary>目次</summary>
     <ul>
-      {#each toc as item, i}
-        {#if i === 0}
-          <li>
-            <a href="#{item.id}">{item.text}</a>
-          </li>
-          {updateTmpItemName(item.name)}
-          a
-        {:else if tmpItemName === item.name}
-          {incrementCount()}
-        {:else if tmpItemName !== item.name}
-          {clearCount()}
-        {/if}
+      {#each toc as item}
+        <li>
+          <a href="#{item.id}" class={item.name}>{item.text}</a>
+        </li>
       {/each}
     </ul>
   </details>
