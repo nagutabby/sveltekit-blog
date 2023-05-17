c209b0b3255d8eb97be184a2a17a8526a2c081b9
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-05-17 17:28:58 +0900

    TOCを途中まで作る

---
+++ src/lib/components/Toc.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let toc: { text: any; id: string; name: string }[];
+
+  let tmpItemName = "";
+  let count = 0;
+  function clearCount() {
+    count = 0;
+  }
+  function incrementCount() {
+    count += 1;
+  }
+  function updateTmpItemName(itemName: string) {
+    tmpItemName = itemName;
+  }
+</script>
+
+<article>
+  <details>
+    <summary>目次</summary>
+    <ul>
+      {#each toc as item, i}
+        {#if i === 0}
+          <li>
+            <a href="#{item.id}">{item.text}</a>
+          </li>
+          {updateTmpItemName(item.name)}
+          a
+        {:else if tmpItemName === item.name}
+          {incrementCount()}
+        {:else if tmpItemName !== item.name}
+          {clearCount()}
+        {/if}
+      {/each}
+    </ul>
+  </details>
+</article>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
   import Breadcrumb from "$lib/components/Breadcrumb.svelte";
+  import Toc from "$lib/components/Toc.svelte";
   import type { PageData } from "./$types";
   export let data: PageData;
+
+  import { load } from "cheerio";
+
+  const loadedBody = load(data.body);
+  const headings = loadedBody("h1, h2, h3").toArray();
+  const toc = headings.map((data) => ({
+    text: (data.children[0] as any).data,
+    id: data.attribs.id,
+    name: data.name,
+  }));
 </script>
 
 <Breadcrumb title={data.title} />
+
+<Toc {toc} />
 
 <article>
   {@html data.body}
