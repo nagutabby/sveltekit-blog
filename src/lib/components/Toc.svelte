12ee3973b484976cb09bdf3461c50a063610a117
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-05-18 02:43:10 +0900

    目次にインデントを追加する

--- src/lib/components/Toc.svelte
+++ src/lib/components/Toc.svelte
@@ -7,10 +7,41 @@
     <summary>目次</summary>
     <ul>
       {#each toc as item}
-        <li>
-          <a href="#{item.id}" class={item.name}>{item.text}</a>
+        <li class={item.name}>
+          <a href="#{item.id}">{item.text}</a>
         </li>
       {/each}
     </ul>
   </details>
 </article>
+
+<style>
+  ul {
+    padding: 0;
+    margin-bottom: 0;
+  }
+
+  details {
+    border: none;
+    padding-bottom: 0;
+    margin-bottom: 0;
+  }
+
+  li::before {
+    content: "•";
+    position: relative;
+    top: 2px;
+    padding-right: 5px;
+    font-size: 1.3rem;
+  }
+  li {
+    list-style-type: none;
+    margin: 0;
+  }
+  .h2 {
+    padding-left: 1.2rem;
+  }
+  .h3 {
+    padding-left: 2.4rem;
+  }
+</style>
