723db46f362077f0f079c0ba67d8f6a0b542c20c
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-11-03 16:27:45 +0900

    Tocコンポーネントを呼び出す際の引数を削除する

--- src/lib/components/Toc.svelte
+++ src/lib/components/Toc.svelte
@@ -1,11 +1,75 @@
 <script lang="ts">
-  export let toc: string;
+  import { onMount } from "svelte";
+
+  let rawToc: string = "";
+
+  onMount(async () => {
+    let headings: any = Array.from(
+      document.getElementById("content")!.querySelectorAll("h1, h2, h3")
+    );
+    const toc = headings.map((heading: Element) => ({
+      text: heading.textContent,
+      id: heading.getAttribute("id"),
+      name: heading.tagName,
+    }));
+    let previousTag: string = "";
+    for (let i = 0; i < toc.length; i++) {
+      if (previousTag === "") {
+        rawToc += `<ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
+      } else if (toc[i].name === previousTag) {
+        rawToc += `<li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
+      } else if (previousTag === "H1") {
+        if (toc[i].name === "H2" || "H3") {
+          rawToc += `<ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
+        }
+      } else if (previousTag === "H2") {
+        if (toc[i].name === "H3") {
+          rawToc += `<ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
+        } else if (toc[i].name === "H1") {
+          rawToc += `</ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
+        }
+      } else if (previousTag === "H3") {
+        if (toc[i].name === "H2") {
+          rawToc += `</ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
+        } else if (toc[i].name === "H1") {
+          rawToc += `</ul></ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
+        }
+      }
+      previousTag = toc[i].name;
+    }
+    let activeHeading: any;
+    const updateElements = (entries: any) => {
+      entries.forEach((entry: any) => {
+        if (entry.isIntersecting) {
+          if (activeHeading !== undefined && activeHeading !== null) {
+            activeHeading.classList.remove("active");
+          }
+          activeHeading = document.querySelector(
+            `a[href="#${entry.target.id}"]`
+          );
+
+          if (activeHeading !== null) {
+            activeHeading.classList.add("active");
+          }
+        }
+      });
+    };
+    headings = document.querySelectorAll("h1, h2, h3");
+    const options = {
+      rootMargin: "0% 0% -100% 0%",
+      threshold: 0,
+    };
+    const observer = new IntersectionObserver(updateElements, options);
+    headings.forEach((heading: any) => {
+      observer.observe(heading);
+    });
+  });
 </script>
 
 <article>
   <details open>
     <summary>目次</summary>
-    {@html toc}
+    {@html rawToc}
   </details>
 </article>
 
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,75 +1,10 @@
 <script lang="ts">
   import Toc from "$lib/components/Toc.svelte";
   import type { PageData } from "./$types";
-  import { onMount } from "svelte";
   import Breadcrumb from "$lib/components/Breadcrumb.svelte";
   import TagDropdown from "$lib/components/TagDropdown.svelte";
   import ShareButton from "$lib/components/ShareButton.svelte";
   export let data: PageData;
-
-  let rawToc: string = "";
-
-  onMount(async () => {
-    let headings: any = Array.from(
-      document.getElementById("content")!.querySelectorAll("h1, h2, h3")
-    );
-    const toc = headings.map((heading: Element) => ({
-      text: heading.textContent,
-      id: heading.getAttribute("id"),
-      name: heading.tagName,
-    }));
-    let previousTag: string = "";
-    for (let i = 0; i < toc.length; i++) {
-      if (previousTag === "") {
-        rawToc += `<ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
-      } else if (toc[i].name === previousTag) {
-        rawToc += `<li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
-      } else if (previousTag === "H1") {
-        if (toc[i].name === "H2" || "H3") {
-          rawToc += `<ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
-        }
-      } else if (previousTag === "H2") {
-        if (toc[i].name === "H3") {
-          rawToc += `<ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
-        } else if (toc[i].name === "H1") {
-          rawToc += `</ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
-        }
-      } else if (previousTag === "H3") {
-        if (toc[i].name === "H2") {
-          rawToc += `</ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
-        } else if (toc[i].name === "H1") {
-          rawToc += `</ul></ul><li><a href="#${toc[i].id}">${toc[i].text}</a></li>`;
-        }
-      }
-      previousTag = toc[i].name;
-    }
-    let activeHeading: any;
-    const updateElements = (entries: any) => {
-      entries.forEach((entry: any) => {
-        if (entry.isIntersecting) {
-          if (activeHeading !== undefined && activeHeading !== null) {
-            activeHeading.classList.remove("active");
-          }
-          activeHeading = document.querySelector(
-            `a[href="#${entry.target.id}"]`
-          );
-
-          if (activeHeading !== null) {
-            activeHeading.classList.add("active");
-          }
-        }
-      });
-    };
-    headings = document.querySelectorAll("h1, h2, h3");
-    const options = {
-      rootMargin: "0% 0% -100% 0%",
-      threshold: 0,
-    };
-    const observer = new IntersectionObserver(updateElements, options);
-    headings.forEach((heading: any) => {
-      observer.observe(heading);
-    });
-  });
 </script>
 
 <Breadcrumb title={data.title} />
@@ -84,7 +19,7 @@
     <div class="sticky-top">
       <div class="col-12">
         <div class="toc">
-          <Toc toc={rawToc} />
+          <Toc />
         </div>
       </div>
       <div class="col-12">
