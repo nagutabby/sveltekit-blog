c148f0ea61152de77dd781e1395c1144721180c9
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-11-12 22:07:26 +0900

    atom:linkタグを追加する

--- src/routes/atom.xml/+server.ts
+++ src/routes/atom.xml/+server.ts
@@ -31,7 +31,8 @@ export async function GET({ setHeaders }) {
   const posts = response.contents.map((post) => create_entry(post.title, post.description, post.id, post.createdAt, post.revisedAt));
 
   const atom = `<?xml version="1.0" encoding="UTF-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="ja">
+<feed xmlns:atom="http://www.w3.org/2005/Atom">
+<atom:link href="/atom.xml" rel="self" type="application/rss+xml" />
 <title>nagutabbyの考え事</title>
 <link href="https://blog.nagutabby.uk" />
 <updated>${latestDate.toISOString()}</updated>
