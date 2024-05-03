fdf827da6151daffc456b84b78ae5e5c35f73ff5
nagutabby <62084485+nagutabby@users.noreply.github.com>
2024-05-04 04:33:34 +0900

    コードのコピー機能を追加する

--- src/routes/(blog)/[slug]/+page.svelte
+++ src/routes/(blog)/[slug]/+page.svelte
@@ -36,6 +36,36 @@
     document.querySelectorAll("pre code").forEach((element) => {
       hljs.highlightElement(element as HTMLElement);
     });
+    document.querySelectorAll("pre").forEach((element) => {
+      const copyButton = document.createElement("button");
+      element.insertAdjacentElement("beforeend", copyButton)
+      copyButton.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+        <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+        <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+      </svg>`;
+      copyButton.setAttribute("data-tooltip", "コピー");
+      copyButton.setAttribute("data-placement", "left");
+      copyButton.addEventListener("click", async (event) => {
+        const code = copyButton.nextElementSibling?.textContent!;
+
+        navigator.clipboard.writeText(code).then(() => {
+          copyButton.innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+            <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+          </svg>`;
+          copyButton.setAttribute("data-tooltip", "コピーしました！")
+          setTimeout(() => {
+            copyButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+              <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+              <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+            </svg>`;
+            copyButton.setAttribute("data-tooltip", "コピー");
+          }, 3000);
+        })
+      })
+    })
   })
 </script>
 
