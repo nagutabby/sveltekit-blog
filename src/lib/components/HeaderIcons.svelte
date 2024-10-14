60cb9c7f666e65726c328e47931c6f45fe7c4b77
nagutabby <62084485+nagutabby@users.noreply.github.com>
2024-10-14 18:14:46 +0900

    アクセシビリティーを改善する

--- src/lib/components/HeaderIcons.svelte
+++ src/lib/components/HeaderIcons.svelte
@@ -39,21 +39,24 @@
 </script>
 
 <div class="header-icons">
-  <a href="/">
-    <button class="contrast">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        class="bi bi-house"
-        viewBox="0 0 16 16"
-      >
-        <path
-          d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"
-        />
-      </svg>
-    </button>
+  <a href="/" class="contrast" role="button" aria-label="Home">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      class="bi bi-house"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"
+      />
+    </svg>
   </a>
-  <button class="contrast" data-target="search-modal" id="search-button">
+  <button
+    class="contrast"
+    data-target="search-modal"
+    id="search-button"
+    aria-label="Search"
+  >
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
@@ -92,13 +95,15 @@
     display: flex;
     justify-content: space-between;
 
-    & button {
+    & a,
+    button {
       border-radius: 50%;
       min-width: 60px;
       width: 5vw;
       aspect-ratio: 1 / 1;
       display: flex;
       justify-content: center;
+      align-items: center;
       opacity: 0.9;
       & svg {
         width: 100%;
