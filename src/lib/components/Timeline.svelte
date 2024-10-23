2df4e5381b90576aaab0482b6c8deec1ea8b6e54
nagutabby <62084485+nagutabby@users.noreply.github.com>
2024-10-24 04:03:14 +0900

    Mastodonのタイムラインを追加する

---
+++ src/lib/components/Timeline.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import * as MastodonTimeline from "@idotj/mastodon-embed-timeline";
+  import "@idotj/mastodon-embed-timeline/dist/mastodon-timeline.min.css";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    new MastodonTimeline.Init({
+      instanceUrl: "https://mastodon.social",
+      timelineType: "profile",
+      userId: "111085130737386025",
+      profileName: "@nagutabby",
+      maxNbPostFetch: "10",
+      maxNbPostShow: "10",
+      dateLocale: "ja-JP",
+      btnSeeMore: "",
+      btnReload: "",
+      defaultTheme: "dark",
+    });
+  });
+</script>
+
+<div id="mt-container" class="mt-container">
+  <div class="mt-body" role="feed">
+    <article aria-busy="true" class="loading-article"></article>
+  </div>
+</div>
+
+<style lang="scss">
+  .mt-container {
+    background-color: var(--pico-card-background-color);
+    padding: 0.5rem;
+    border-radius: 5px;
+    scrollbar-color: var(--pico-muted-color) var(--pico-card-background-color);
+  }
+
+  .mt-body {
+    position: relative;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column;
+  }
+  .loading-article {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,98 +3,115 @@
   import Card from "$lib/components/Card.svelte";
   import Pagination from "$lib/components/Pagination.svelte";
   import { page } from "$app/stores";
+  import Timeline from "$lib/components/Timeline.svelte";
 
   export let data: PageData;
 
   let currentPage = Number($page.url.searchParams.get("page")) || 1;
 </script>
 
-{#if data.contents.length % 3 == 0}
-  {#each data.contents as { }, i}
-    {#if (i + 1) % 3 == 0}
-      <div class="grid">
-        <Card
-          url={data.contents[i - 2].id}
-          image={data.contents[i - 2].image.url}
-          title={data.contents[i - 2].title}
-        />
-        <Card
-          url={data.contents[i - 1].id}
-          image={data.contents[i - 1].image.url}
-          title={data.contents[i - 1].title}
-        />
-        <Card
-          url={data.contents[i].id}
-          image={data.contents[i].image.url}
-          title={data.contents[i].title}
-        />
-      </div>
+<div class="main-content">
+  <div class="article-card">
+    {#if data.contents.length % 3 == 0}
+      {#each data.contents as { }, i}
+        {#if (i + 1) % 3 == 0}
+          <div class="grid">
+            <Card
+              url={data.contents[i - 2].id}
+              image={data.contents[i - 2].image.url}
+              title={data.contents[i - 2].title}
+            />
+            <Card
+              url={data.contents[i - 1].id}
+              image={data.contents[i - 1].image.url}
+              title={data.contents[i - 1].title}
+            />
+            <Card
+              url={data.contents[i].id}
+              image={data.contents[i].image.url}
+              title={data.contents[i].title}
+            />
+          </div>
+        {/if}
+      {/each}
+    {:else if data.contents.length % 3 == 2}
+      {#each data.contents as { }, i}
+        {#if (i + 1) % 3 == 0}
+          <div class="grid">
+            <Card
+              url={data.contents[i - 2].id}
+              image={data.contents[i - 2].image.url}
+              title={data.contents[i - 2].title}
+            />
+            <Card
+              url={data.contents[i - 1].id}
+              image={data.contents[i - 1].image.url}
+              title={data.contents[i - 1].title}
+            />
+            <Card
+              url={data.contents[i].id}
+              image={data.contents[i].image.url}
+              title={data.contents[i].title}
+            />
+          </div>
+        {:else if i + 1 == data.contents.length}
+          <div class="grid">
+            <Card
+              url={data.contents[i - 1].id}
+              image={data.contents[i - 1].image.url}
+              title={data.contents[i - 1].title}
+            />
+            <Card
+              url={data.contents[i].id}
+              image={data.contents[i].image.url}
+              title={data.contents[i].title}
+            />
+          </div>
+        {/if}
+      {/each}
+    {:else if data.contents.length % 3 == 1}
+      {#each data.contents as { }, i}
+        {#if (i + 1) % 3 == 0}
+          <div class="grid">
+            <Card
+              url={data.contents[i - 2].id}
+              image={data.contents[i - 2].image.url}
+              title={data.contents[i - 2].title}
+            />
+            <Card
+              url={data.contents[i - 1].id}
+              image={data.contents[i - 1].image.url}
+              title={data.contents[i - 1].title}
+            />
+            <Card
+              url={data.contents[i].id}
+              image={data.contents[i].image.url}
+              title={data.contents[i].title}
+            />
+          </div>
+        {:else if i + 1 == data.contents.length}
+          <div class="grid">
+            <Card
+              url={data.contents[i].id}
+              image={data.contents[i].image.url}
+              title={data.contents[i].title}
+            />
+          </div>
+        {/if}
+      {/each}
     {/if}
-  {/each}
-{:else if data.contents.length % 3 == 2}
-  {#each data.contents as { }, i}
-    {#if (i + 1) % 3 == 0}
-      <div class="grid">
-        <Card
-          url={data.contents[i - 2].id}
-          image={data.contents[i - 2].image.url}
-          title={data.contents[i - 2].title}
-        />
-        <Card
-          url={data.contents[i - 1].id}
-          image={data.contents[i - 1].image.url}
-          title={data.contents[i - 1].title}
-        />
-        <Card
-          url={data.contents[i].id}
-          image={data.contents[i].image.url}
-          title={data.contents[i].title}
-        />
-      </div>
-    {:else if i + 1 == data.contents.length}
-      <div class="grid">
-        <Card
-          url={data.contents[i - 1].id}
-          image={data.contents[i - 1].image.url}
-          title={data.contents[i - 1].title}
-        />
-        <Card
-          url={data.contents[i].id}
-          image={data.contents[i].image.url}
-          title={data.contents[i].title}
-        />
-      </div>
-    {/if}
-  {/each}
-{:else if data.contents.length % 3 == 1}
-  {#each data.contents as { }, i}
-    {#if (i + 1) % 3 == 0}
-      <div class="grid">
-        <Card
-          url={data.contents[i - 2].id}
-          image={data.contents[i - 2].image.url}
-          title={data.contents[i - 2].title}
-        />
-        <Card
-          url={data.contents[i - 1].id}
-          image={data.contents[i - 1].image.url}
-          title={data.contents[i - 1].title}
-        />
-        <Card
-          url={data.contents[i].id}
-          image={data.contents[i].image.url}
-          title={data.contents[i].title}
-        />
-      </div>
-    {:else if i + 1 == data.contents.length}
-      <div class="grid">
-        <Card
-          url={data.contents[i].id}
-          image={data.contents[i].image.url}
-          title={data.contents[i].title}
-        />
-      </div>
-    {/if}
-  {/each}
-{/if}
+  </div>
+  <div class="timeline">
+    <Timeline></Timeline>
+  </div>
+</div>
 <Pagination totalArticles={data.totalCount} />
+
+<style lang="scss">
+  .main-content {
+    display: flex;
+    flex-flow: column;
+    justify-content: space-between;
+    margin: 1rem 0;
+  }
+</style>
