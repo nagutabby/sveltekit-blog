5637d559b37e2e9b8f8323245bdc05064a529e07
nagutabby <62084485+nagutabby@users.noreply.github.com>
2025-01-13 04:29:52 +0900

    ディレクトリ名を変更する

--- src/routes/[name]/+page.ts
+++ src/routes/[name]/+page.ts
@@ -2,7 +2,7 @@ import { getDetail } from "$lib/microcms";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ params }) => {
-  const articleData = await getDetail(params.slug);
+  const articleData = await getDetail(params.name);
   const data = {
     ...articleData,
   };
