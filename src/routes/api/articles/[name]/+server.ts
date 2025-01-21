86813b4e4d7900caaed2337d485d8b2ec2a120e8
nagutabby <62084485+nagutabby@users.noreply.github.com>
2025-01-22 05:57:18 +0900

    /api/[name]にアクセスされたときにNoteオブジェクトを返す

--- src/routes/api/articles/[name]/+server.ts
+++ src/routes/api/articles/[name]/+server.ts
@@ -1,11 +1,33 @@
 import type { RequestHandler } from '@sveltejs/kit';
+import { getDetail, type Blog } from '$lib/microcms';
 
-export const GET: RequestHandler = async ({ params, request }) => {
-  const acceptHeader = request.headers.get('Accept');
-  if (acceptHeader) {
-    console.log('acceptHeader: ', acceptHeader);
+export const GET: RequestHandler = async ({ params }) => {
+  const { name } = params;
+  let articleData: Blog | null;
+
+  // 記事データの取得
+  if (name) {
+    articleData = await getDetail(name);
+    if (!articleData) {
+      return new Response('Article not found', { status: 404 });
+    }
+  } else {
+    return new Response('Article not found', { status: 404 });
   }
-  return new Response(null, {
+
+  // Noteオブジェクトの定義
+  const note = {
+    "id": `https://blog.nagutabby.uk/api/articles/${name}`,
+    "type": "Note",
+    "attributedTo": "https://blog.nagutabby.uk/actor",
+    "name": articleData.title,
+    "content": `<p>${articleData.title}</p><a href="https://blog.nagutabby.uk/articles/${name}" target="_blank">https://blog.nagutabby.uk/articles/${name}</a>`,
+    "published": articleData.publishedAt,
+    "url": `https://blog.nagutabby.uk/api/articles/${name}`,
+    "to": ["https://www.w3.org/ns/activitystreams#Public"]
+  };
+
+  return new Response(JSON.stringify(note), {
     status: 200,
     headers: {
       'Content-Type': 'application/activity+json'
