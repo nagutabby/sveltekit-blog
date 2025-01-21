ad1fabc70d22f45c9364d919699c67a53220be3f
nagutabby <62084485+nagutabby@users.noreply.github.com>
2025-01-22 01:09:24 +0900

    Createアクティビティを送信するAPIを追加する

---
+++ src/routes/api/activitypub/create/+server.ts
@@ -0,0 +1,12 @@
+import prisma from '$lib/prisma';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const POST: RequestHandler = async ({ request, fetch }) => {
+  const data = await request.json();
+
+  if (data.id) {
+    const response = await fetch(`/articles/${data.id}/create`);
+    console.log(response.json)
+  }
+  return new Response(null, { status: 200 });
+}
