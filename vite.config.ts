7f62719e5b241966bc447e3c86c02e260d3043c3
nagutabby <nagutabby@nagutabby.uk>
2025-04-07 04:59:18 +0900

    不要なコードを削除する

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vitest/config";
 import { sveltekit } from "@sveltejs/kit/vite";
-import { dirname, join } from 'path';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { svelteTesting } from '@testing-library/svelte/vite';
-
-const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   // @ts-expect-error
