c3e95917b85d59149e151d25fa273ed117e661b1
nagutabby <nagutabby@nagutabby.uk>
2025-05-03 14:22:17 +0900

    erbとscssモジュールを削除し、xmlモジュールを追加する

--- src/routes/articles/[name]/+page.svelte
+++ src/routes/articles/[name]/+page.svelte
@@ -2,10 +2,9 @@
   import hljs from "highlight.js/lib/core";
   import python from "highlight.js/lib/languages/python";
   import ruby from "highlight.js/lib/languages/ruby";
-  import erb from "highlight.js/lib/languages/erb";
   import bash from "highlight.js/lib/languages/bash";
+  import xml from "highlight.js/lib/languages/xml";
   import css from "highlight.js/lib/languages/css";
-  import scss from "highlight.js/lib/languages/scss";
   import javascript from "highlight.js/lib/languages/javascript";
   import typescript from "highlight.js/lib/languages/typescript";
   import yaml from "highlight.js/lib/languages/yaml";
@@ -24,10 +23,10 @@
 
   hljs.registerLanguage("python", python);
   hljs.registerLanguage("ruby", ruby);
-  hljs.registerLanguage("erb", erb);
   hljs.registerLanguage("bash", bash);
+  hljs.registerLanguage("html", xml);
+  hljs.registerLanguage("xml", xml);
   hljs.registerLanguage("css", css);
-  hljs.registerLanguage("scss", scss);
   hljs.registerLanguage("javascript", javascript);
   hljs.registerLanguage("typescript", typescript);
   hljs.registerLanguage("json", json);
