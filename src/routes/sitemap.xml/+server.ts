0c1d0758330b5ecb2bafb7ce2312dbebdf84363d
nagutabby <nagutabby@nagutabby.uk>
2025-06-29 07:13:39 +0900

    サイトマップを更新する

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,8 +1,9 @@
+import type { Article, Review } from '$lib/types/blog.js';
 import { getAllRawData } from '$lib/utils.js';
 
-function create_entry(path: string, lastmod: Date) {
+function createEntry(path: string, lastmod: Date, type: "articles" | "reviews") {
   return `<url>
-    <loc>${new URL(`articles/${path}`, "https://blog.nagutabby.uk").href}</loc>
+    <loc>${new URL(`${type}/${path}`, "https://blog.nagutabby.uk").href}</loc>
     ${lastmod ? `<lastmod>${lastmod.toISOString()}</lastmod>` : ''}
   </url>`;
 }
@@ -12,10 +13,14 @@ export async function GET({ setHeaders }) {
     'Content-Type': 'application/xml'
   });
 
-  const allArticles = await getAllRawData("articles")
+  const allArticles = await getAllRawData("articles") as Article[]
+  const allReviews = await getAllRawData("reviews") as Review[]
 
-  const posts = allArticles.map((post) => create_entry(post.id, post.updatedAt));
+  const articlePosts = allArticles.map((post) => createEntry(post.id, post.updatedAt, "articles"));
+  const reviewPosts = allReviews.map((post) => createEntry(post.id, post.updatedAt, "reviews"));
 
+  const posts = [...articlePosts, ...reviewPosts]
+  
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${posts.join('\n')}
