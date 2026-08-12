43fbe71db9563037ba0c93d3ac83ed308340a96a
Hiroto Sasagawa <nagutabby@nagutabby.uk>
2026-08-12 19:49:56 +0900

    Revert "AWS CDK雛形をinfra/に追加する"

--- infra/lib/dynamodb-stack.ts
+++
@@ -1,9 +0,0 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
-
-// Follower/RelayConnectionテーブルはmigrate/12で追加する。
-export class DynamoDbStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-  }
-}
--- infra/lib/site-stack.ts
+++
@@ -1,9 +0,0 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
-
-// blog.nagutabby.uk向けのS3+CloudFront(SSGサイト+Federationパスのビヘイビア分岐)はmigrate/14で追加する。
-export class SiteStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-  }
-}
